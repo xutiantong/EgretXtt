8531e34750bbbed9a388676d362105adab7439bc
xutiantong <xutiantong@xtt.com>
2020-03-24 18:09:04 +0800

    其他回滚

--- resource/assets/Start/src/Utils/Utils.ts
+++
@@ -1,9 +0,0 @@
-namespace Utils {
-    export function makeArrayRandom(arr: any[]) {
-        for (let i = 0; i < arr.length; ++i) {
-            let j = (arr.length * Math.random()) | 0;
-            if (i != j)
-                [arr[i], arr[j]] = [arr[j], arr[i]];
-        }
-    }
-}
