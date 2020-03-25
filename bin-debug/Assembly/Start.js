eca4d2a3112f6d3b4bdc7b646c0ca9ebf0b39db0
xutiantong <xutiantong@xtt.com>
2020-03-25 21:12:37 +0800

    刷光实验

--- bin-debug/Assembly/Start.js
+++ bin-debug/Assembly/Start.js
@@ -59,6 +59,29 @@ var Assembly;
             this.startAni.animation.play("newAnimation", 1);
             this.startBtnAni.animation.play("normal", 0);
             Manager.EventManager.get().addListener("Start", this.startGroup, egret.TouchEvent.TOUCH_TAP, this.onStart, this);
+            this.addShua(this["gg"]);
+        };
+        Start.prototype.addShua = function (obj) {
+            var texture = new egret.RenderTexture();
+            texture.drawToTexture(obj);
+            var gameTitle_mask = new egret.Bitmap(texture);
+            gameTitle_mask.x = obj.x;
+            gameTitle_mask.y = obj.y;
+            this.addChild(gameTitle_mask);
+            var spark = new egret.Bitmap(RES.getRes("soldier_light_png"));
+            var MOVE_MIN_Y = obj.y;
+            var MOVE_MAX_Y = obj.y + obj.height - spark.height;
+            spark.y = MOVE_MIN_Y;
+            spark.x = obj.x;
+            spark.mask = gameTitle_mask;
+            this.addChild(spark);
+            var speed = 8;
+            var direction = 1;
+            egret.Ticker.getInstance().register(function () {
+                spark.y += speed * direction;
+                if (spark.y > MOVE_MAX_Y || spark.y < MOVE_MIN_Y)
+                    direction *= -1;
+            }, this);
         };
         return Start;
     }(eui.Component));
--- src/Assembly/Start.ts
+++ src/Assembly/Start.ts
@@ -57,8 +57,34 @@ module Assembly {
 			this.startAni.animation.play("newAnimation", 1)
 			this.startBtnAni.animation.play("normal", 0)
 			Manager.EventManager.get().addListener("Start", this.startGroup, egret.TouchEvent.TOUCH_TAP, this.onStart, this);
+			this.addShua(this["gg"]);
 		}
 
+		private addShua(obj: egret.DisplayObject) {
+			let texture = new egret.RenderTexture();
+			texture.drawToTexture(obj);
+
+			var gameTitle_mask: egret.Bitmap = new egret.Bitmap(texture);
+			gameTitle_mask.x = obj.x;
+			gameTitle_mask.y = obj.y;
+			this.addChild(gameTitle_mask);
+
+			var spark: egret.Bitmap = new egret.Bitmap(RES.getRes("soldier_light_png"));
+			var MOVE_MIN_Y = obj.y;
+			var MOVE_MAX_Y = obj.y + obj.height - spark.height;
+			spark.y = MOVE_MIN_Y;
+			spark.x = obj.x;
+			spark.mask = gameTitle_mask;
+			this.addChild(spark);
+
+			var speed: number = 8;
+			var direction: number = 1;
+
+			egret.Ticker.getInstance().register(function () {
+				spark.y += speed * direction;
+				if (spark.y > MOVE_MAX_Y || spark.y < MOVE_MIN_Y) direction *= -1;
+			}, this);
+		}
 
 	}
 }
